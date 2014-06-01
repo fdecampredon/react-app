bffa0f99fe65e5e9b173d262619b382ec97169b6
François de Campredon <francois.de.campredon@gmail.com>
2014-06-01 12:31:34 +0200

    don't use react for full page rendering

--- lib/views/mainView.jsx
+++ lib/views/mainView.jsx
@@ -7,33 +7,23 @@ var AppHeader       = require('./appHeader.jsx');
 var SessionStore    = require('../store/sessionStore');
 var MainView = React.createClass({
     contextTypes: {
-      isServer: React.PropTypes.bool,
-      currentRequest : React.PropTypes.object
+        isServer: React.PropTypes.bool,
+        currentRequest : React.PropTypes.object
     },
     
     render: function() {
         var currentUser;
         if (this.context.isServer) {
-            var currentUser = this.context.currentRequest.user;
+            currentUser = this.context.currentRequest.user;
             console.log(currentUser);
-        } else {
-            var currentUser = null;
         }
         return (
-            <html>
-                <head>
-                    <title>Task Manager</title>
-                    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0-rc-1/pure-min.css" />
-                    <link rel='stylesheet' href='/style.css' />
-                </head>
-                <body>
-                    <AppHeader currentUser={currentUser} />
-                    <div className='page-content'>
-                        {this.props.contentView}
-                    </div>
-                    <script src='/bundle.js' />
-                </body>
-            </html>
+            <div>
+                <AppHeader currentUser={currentUser} />
+                <div className='page-content'>
+                    {this.props.contentView}
+                </div>
+            </div>
         );
     }
 });
